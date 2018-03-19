c06fa2605e07a4c4ee0f1d5496d775f2ed0092f0
Paweł Zadrożny <pawel.zny@gmail.com>
2018-03-19 15:25:04 +0100

    Bump version: 0.1.0 → 0.1.1

--- eeee/__init__.py
+++ eeee/__init__.py
@@ -5,5 +5,5 @@ from cl import Loop
 
 __author__ = 'Paweł Zadrożny'
 __copyright__ = 'Copyright (c) 2017, Pawelzny'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __all__ = ['Event', 'Loop', 'Publisher', 'subscribe']
